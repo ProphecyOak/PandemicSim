0cecb9bade9da4f36ba19e9160c47e5dcaac2503
ProphecyOak <drito610@gmail.com>
2020-07-10 21:16:42 -0400

    Canvas in Scene

--- displayData.py
+++ displayData.py
@@ -1,1 +1,7 @@
 import tkinter
+
+class Scene:
+    def __init__(self, width=500, height=500):
+        self.master = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.master, width=width, height=height)
+        self.canvas.pack()
