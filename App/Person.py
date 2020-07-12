77a8aae327ad1819cdb3addf70c86572f6263ee9
Faith217 <68133840+Faith217@users.noreply.github.com>
2020-07-12 11:43:13 -0400

    Update Person.py

--- App/Person.py
+++ App/Person.py
@@ -5,14 +5,15 @@ from math import *
 class Person:
     num = 0
     radius = 5
-    colors = ["deepskyblue2","red3"]
+    colors = ["deepskyblue2","red3", "grey70"]
     def __init__(self, can, xMax, yMax):
         self.x = random.randint(20,xMax-20)
         self.y = random.randint(20,yMax-20)
         self.color = Person.colors[0]
         self.dot = can.create_oval(self.x-Person.radius, self.y-Person.radius, self.x+Person.radius, self.y+Person.radius, fill=self.color)
         self.lastMove = [0,random.random()*360]
-        self.health = 0 #0 is healthy, 1 is infected, and 2 is clear
+        self.health = 0 #0 is healthy, 1 is infected, 2 is clear, and 3 is dead
+        self.recoveryTime = 0
         Person.num += 1
 
     def colorChange(self, can, Grp=1):
