2e9f59e0460df84d44318a80662816dff3cc028d
ProphecyOak <drito610@gmail.com>
2020-07-12 14:48:59 -0400

    Restart Graph

--- App/SideGraph.py
+++ App/SideGraph.py
@@ -27,19 +27,4 @@ class SideGraph:
             self.curX = self.width-10
         startX = self.curX
         endX = self.curX + 10
-        startY = 10
-        endY = self.height-10
         total = healthy + infected + recovered + dead
-        healthy = infected/total
-        infected = healthy/total
-        recovered = recovered/total
-        dead = dead/total
-        healthyY = (self.height-20)*healthy
-        infectedY = healthyY+(self.height-20)*infected
-        recoveredY = infectedY+(self.height-20)*recovered
-        print("\n"+str(healthy)[:4],str(infected)[:4],str(recovered)[:4],str(dead)[:4],"   ",str(total)[:4])
-        print(str(healthyY)[:6],str(infectedY)[:6],str(recoveredY)[:6])
-        self.plotPoints.append([self.createWonkRectangle(self.height,startX,startY,endX,healthyY,fill=SideGraph.colors[1])])#, self.createWonkRectangle(self.height,startX, healthyY, endX, infectedY,fill=SideGraph.colors[0]), self.createWonkRectangle(self.height,startX, infectedY, endX, recoveredY,fill=SideGraph.colors[2]), self.createWonkRectangle(self.height,startX, recoveredY, endX, endY,fill=SideGraph.colors[3])])
-        self.curX += 15
-    def createWonkRectangle(self, axisHeight, x1, y1, x2, y2, **kwargs):
-        return self.canvas.create_rectangle(x1, axisHeight-y1+10, x2, axisHeight-y2, **kwargs)
